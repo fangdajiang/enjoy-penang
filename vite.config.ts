c29ce8157fd7e412ef157c9ba899180398bda978
fangdajiang <fang.dajiang@gmail.com>
2025-03-03 14:35:27 +0800

    init2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 使用相对路径，使得直接打开 HTML 文件时资源能够正确加载
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
